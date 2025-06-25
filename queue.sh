9aa9679c2bd797cd61eb17ee21f4c9ccaecc37f2
Evan Rantala <evanrantala@Evans-Laptop.attlocal.net>
2025-06-24 17:35:30 -0700

    small change

--- queue.sh
+++ queue.sh
@@ -1,7 +1,7 @@
 #!/bin/bash  
 ### Job Name
-#PBS -N FNO_Euler_find_grads_1k
-#PBS -e FNO_Euler_find_grads_1k.err
+#PBS -N FNO_Implicit_Euler_find_grads_1k
+#PBS -e FNO_Implicit_Euler_find_grads_1k.err
 ### Charging account
 #PBS -A UCSC0009
 #PBS -l walltime=12:00:00
@@ -11,8 +11,9 @@
 #PBS -l gpu_type=a100
 
 module load conda
-#conda activate /glade/work/cainslie/conda-envs/cainslie_env
-conda activate /glade/work/erantala/conda-envs/erantala_env
-cd /glade/derecho/scratch/cainslie/conrad_net_stability/Conrad_Research_4_Ashesh/
+source $(conda info --base)/etc/profile.d/conda.sh
+conda activate /glade/work/erantala/conda-envs/jacobian_env
+
+cd /glade/derecho/scratch/erantala/project_runs/code
 
 python -u -m eval_w_jacs.py
